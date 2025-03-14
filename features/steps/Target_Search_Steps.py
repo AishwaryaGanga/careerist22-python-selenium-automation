62e42a35259c3ef4af42fb2b93d0a599a8f366ab
Aishwarya <aishwaryaganga.s@gmail.com>
2025-03-14 11:43:30 -0400

    updated target_search

---
+++ features/steps/Target_Search_Steps.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+
+
+
+#from sample_script import driver
+
+CART_BUTTON = (By.CSS_SELECTOR,"[data-test = '@web/CartLink']")
+PAGE_TEXT =  (By.CSS_SELECTOR, ".styles_fontSize1__i0fbt")
+
+#practice purpose (By.XPATH, "//a[@data-test = '@web/CartLink']")
+
+@given('Open target.com')
+def open_google(context):
+    context.driver.get('https://www.target.com/')
+
+
+@when('Click on the Cart icon')
+def input_search(context):
+    cart = context.driver.find_element(*CART_BUTTON).click()
+    sleep(3)
+
+@then('Verify "You cart is empty"')
+def verify_empty(context):
+    page_text_element = context.driver.find_element(*PAGE_TEXT)
+    assert "Your cart is empty" in page_text_element.text, \
+        f'Expected text "Your cart is empty" not found in {page_text_element.text}'
+    print('Verified "Your cart is empty"')
---
+++ features/tests/Target_Search.feature
@@ -0,0 +1,7 @@
+# Created by vaishnavisethuraman at 13/03/25
+Feature: Test Scenarios for Search functionality
+
+  Scenario: User verify the Cart button
+    Given Open target.com
+    When Click on the Cart icon
+    Then Verify "You cart is empty"
