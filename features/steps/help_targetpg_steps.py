a13443a11202c2c2da4e5e1b508468009b8b7964
Aishwarya <aishwaryaganga.s@gmail.com>
2025-03-18 15:44:43 -0400

    help UI updated

---
+++ features/steps/help_targetpg_steps.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+
+LINKS = (By.CSS_SELECTOR, ".container.clearfix")
+
+@given("Open target help page")
+def help_pg(context):
+    context.driver.get("https://help.target.com/help")
+    sleep(2)
+
+@then("Verify the {link_count} links")
+def verify_links(context, link_count):
+    link_count = int(link_count)
+    links = context.driver.find_elements(*LINKS)
+    print(links)
+    assert len(links) == link_count,f'Expected {link_count} links, but got {len(links)}'
---
+++ features/tests/help_target_page.feature
@@ -0,0 +1,6 @@
+# Created by vaishnavisethuraman at 18/03/25
+Feature: Target help UI test
+
+  Scenario: Verify all header links shown
+    Given Open target help page
+    Then Verify the 6 links
